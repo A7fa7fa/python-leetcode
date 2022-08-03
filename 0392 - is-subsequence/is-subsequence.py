67cf5a1f41743750977a85175dffa43da83b0979
A7fa7fa <manuel.seidler@daimler.com>
2022-08-03 16:38:31 +0200

    feat/ is subsequence

---
+++ 0392 - is-subsequence/is-subsequence.py
@@ -0,0 +1,12 @@
+def isSubsequence(s: str, t: str) -> bool:
+	if len(s) == 0:
+		return True
+	pointer = 0
+	for char in t:
+		if char == s[pointer]:
+			pointer += 1
+		if pointer == len(s):
+			return True
+	
+	return False
+	
