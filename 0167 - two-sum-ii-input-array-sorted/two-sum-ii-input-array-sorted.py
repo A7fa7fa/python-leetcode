9908173820a6ff635f2676b2d6690abf12e5a7c8
A7fa7fa <manuel.seidler@daimler.com>
2022-08-04 12:22:33 +0200

    feat/ two sum with sorted input array

---
+++ 0167 - two-sum-ii-input-array-sorted/two-sum-ii-input-array-sorted.py
@@ -0,0 +1,18 @@
+def twoSum(self, numbers: list[int], target: int) -> List[int]:
+	
+	leftIndex = 0
+	rightIndex = len(numbers) - 1
+	
+	while leftIndex < rightIndex:
+		if numbers[leftIndex] + numbers[rightIndex] == target:
+			# returning just index returns fails testcases
+			# asked are the number of elements in array not index
+			return [leftIndex + 1, rightIndex + 1]
+		
+		if numbers[leftIndex] + numbers[rightIndex] > target:
+				rightIndex -= 1
+		else:
+			leftIndex += 1
+		
+
+	
