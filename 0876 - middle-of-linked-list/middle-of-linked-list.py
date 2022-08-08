cd07623e0cc3fe048abaf99a7f58b7a5e17a1533
A7fa7fa <45770886+A7fa7fa@users.noreply.github.com>
2022-08-08 19:30:16 +0200

    feat/ add another solution to middle of linked list

--- 0876 - middle-of-linked-list/middle-of-linked-list.py
+++ 0876 - middle-of-linked-list/middle-of-linked-list.py
@@ -20,6 +20,18 @@ class ListNode(object):
             temp = temp.next
         
         return result[len(result) // 2]
+
+    def middleNode2(self, head):
+        length = 0 # keeps track of total length of nodes
+        middle = head #keeps track of middle node
+        while head: # as long there is a head
+            head = head.next # move forward
+            length += 1 # up length by one
+            
+            if length % 2 == 0: # whenever new length is devisible by 2. half of length has to move forward as well. every two steps of head, middle mmoves es well
+                middle = middle.next
+        
+        return middle
         
 
 if __name__ == "__main__":
