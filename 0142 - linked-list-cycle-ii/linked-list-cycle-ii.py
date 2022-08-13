36893b50b297695aa2ba325043d82396c50a9b65
A7fa7fa <45770886+A7fa7fa@users.noreply.github.com>
2022-08-13 17:45:08 +0200

    feat/ linked list cycle

---
+++ 0142 - linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+
+class Solution:
+	def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+		
+		visitedNodes = dict()
+		
+		while head:
+			if id(head) in visitedNodes:
+				return head
+			
+			visitedNodes[id(head)] = head            
+			head = head.next
+			
+		return None
+			
+			
+			
+			
+			
