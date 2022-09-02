0aefc3c60eebe7a78fbaf61221972f0a897b7d51
A7fa7fa <manuel.seidler@daimler.com>
2022-09-02 13:39:20 +0200

    feat/ avergae of levels in binary tree - bfs approach

--- 0637 - average-of-levels-in-binary-tree/average-of-levels-in-binary-tree-bfs.py
+++ 0637 - average-of-levels-in-binary-tree/average-of-levels-in-binary-tree-bfs.py
@@ -1,45 +1,16 @@
-import collections
-from statistics import mean
-from typing import List, Optional
-
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 class Solution:
-
-    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        #BFS
-        que = [] # first in first out breadth first search
-        que = collections.deque()
-        que.append(root)
-        res = []
-
-        while (que): #loof through every level
-            qLen = len(que)
-            temp = 0
-
-            for _ in range(qLen): 
-                # loop throug every element of current level and extract value. 
-                # than add new child of every element to que
-                # que wil stop before childs. because there are beyond qLen
-                node:TreeNode = que.popleft()
-                temp += node.val
-
-                if node.left:
-                    que.append(node.left)
-                if node.right:
-                    que.append(node.right)
-
-            res.append(temp/qLen)
-
+            
+    def uniquePaths(self, m: int, n: int) -> int:
         
-        return res
+        mat = [[0 for _ in range(n)] for _ in range(m)]
+        mat[0][0] = 1
         
+        for i in range(m):
+            for j in range(n):                
+                if i > 0:
+                    mat[i][j] += mat[i-1][j]        
+                if j > 0:
+                    mat[i][j] += mat[i][j-1]
         
-        
-        
-                
+
+        return mat[m-1][n-1]             
