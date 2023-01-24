c7e339c7500daf91e2ce843e431b52c46c4184e5
A7fa7fa <seidlermanuel@gmail.com>
2023-01-24 09:27:43 +0100

    feat/ town judge

---
+++ 0997 - find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+
+        trustedBy = defaultdict(int)
+        if n == 1:
+            return 1
+
+        for person, trusts in trust:
+            trustedBy[person] -= 1
+            trustedBy[trusts] += 1
+        for i in range(1, n+1):
+            if i in trustedBy and trustedBy[i] == n-1:
+                return i
+        return -1
